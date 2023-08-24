27e1be82eb477b369637fb2f2bce875a4e75b1c6
Anderson Silva <128851529+Andydev0@users.noreply.github.com>
2023-08-24 14:18:46 -0300

    Update discord.py

--- discord.py
+++ discord.py
@@ -16,12 +16,24 @@ exp_per_level = 20
 
 
 user_levels = {}  # Dicionário para armazenar os níveis e xp dos usuários
-thread_start_times = {}
+
+# Lista de canais excluídos onde o sistema de níveis não funcionará
+#excluded_channels = [1143327353519480952, 1143347662054236322, 1143368647390277805, 1143370778767806504, 1143502875071352913]  # Substitua pelos IDs reais dos canais
+
+
 
 # Função para salvar os dados de experiência em um arquivo JSON
 def save_user_levels(filename, user_levels_data):
+    user_data_to_save = {}
+
+    for user_id, data in user_levels_data.items():
+        user_data_to_save[user_id] = {
+            "exp": data["exp"],
+            "level": data["level"]
+        }
+
     with open(filename, 'w') as file:
-        json.dump(user_levels_data, file, indent=4)
+        json.dump(user_data_to_save, file, indent=4)
 
  # Função para carregar os dados de experiência de um arquivo JSON
 def load_user_levels(filename):
@@ -37,35 +49,35 @@ json_filename = 'user_levels.json'
 # Carrega os dados de experiência existentes ou cria um dicionário vazio
 user_levels = load_user_levels(json_filename)       
 
+processed_threads = set()
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
+processed_threads = set()
 
     
-'''@bot.event
-async def on_message(message):
-    if isinstance(message.channel, discord.Thread):
-        if message.author != bot.user:
-            await message.channel.send(f"deu certo sua resposta, {message.author.mention}!")'''
-
 @bot.event
 async def on_thread_create(thread):
-    user_id = thread.owner_id
-    if user_id not in user_levels:
-        user_levels[user_id] = {'exp': 0, 'level': 1}
-    user_levels[user_id]['exp'] += exp_per_thread
-    while user_levels[user_id]['exp'] >= exp_per_level:
-        user_levels[user_id]['exp'] -= exp_per_level
-        user_levels[user_id]['level'] += 1
-        await thread.send(f"Parabéns, <@{user_id}>! Você alcançou o nível {user_levels[user_id]['level']}!")
-        save_user_levels(json_filename, user_levels)
+    if thread.id not in processed_threads:  # Verifica se o tópico já foi processado
+        user_id = thread.owner_id
+        if user_id not in user_levels:
+            user_levels[user_id] = {'exp': 0, 'level': 1}
+        user_levels[user_id]['exp'] += exp_per_thread
+        while user_levels[user_id]['exp'] >= exp_per_level:
+            user_levels[user_id]['exp'] -= exp_per_level
+            user_levels[user_id]['level'] += 1
+            await thread.send(f"Parabéns, <@{user_id}>! Você alcançou o nível {user_levels[user_id]['level']}!")
+            save_user_levels(json_filename, user_levels)
+        processed_threads.add(thread.id)  # Adiciona o tópico ao conjunto de tópicos processados
+
 
 
 
 @bot.event #exp por tempo decorrido
 async def on_message(message):
     if isinstance(message.channel, discord.Thread):
-        if message.author != bot.user:
+        if message.author != bot.user and len(message.content) >= 10:
             user_id = message.author.id
             if user_id not in user_levels:
                 user_levels[user_id] = {'exp': 0, 'level': 1}
@@ -132,7 +144,26 @@ async def dar_xp(ctx, user: discord.User, xp_amount: int):
     else:
         await ctx.send("Você não tem permissão para usar este comando.")
 
+@bot.command()
+async def top(ctx):
+    sorted_users = sorted(user_levels.items(), key=lambda x: x[1]['level'], reverse=True)
+    top_users = sorted_users[:10]  # Pega os 10 primeiros usuários da lista ordenada
+    
+    leaderboard_message = "Top 10 usuários com mais níveis:\n"
+    
+    for index, (user_id, data) in enumerate(top_users, start=1):
+        try:
+            user = await bot.fetch_user(user_id)  # Obtém o objeto de usuário usando o ID
+        except discord.NotFound:
+            user = None
         
-# Substitua 'YOUR_BOT_TOKEN' pelo token real do seu bot
-bot.run('TOKEN')
+        if user:
+            leaderboard_message += f"{index}. {user.mention} - Nível {data['level']}\n"
+        else:
+            leaderboard_message += f"{index}. *Usuário não encontrado* - Nível {data['level']}\n"
+    
+    await ctx.send(leaderboard_message)
+
+
+bot.run('token')
 
